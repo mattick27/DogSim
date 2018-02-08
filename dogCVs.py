4c6b689846f7bb4c56a196f018fb7689f44c3b90
mattck27 <Matichon3149@gmail.com>
2018-02-08 21:26:23 +0700

    final

--- dogCVs.py
+++ dogCVs.py
@@ -3,6 +3,7 @@ from sklearn import  linear_model
 from sklearn.utils import shuffle
 from sklearn.metrics.pairwise import cosine_similarity as cs
 import numpy as np
+import math
 import os
 import matplotlib.pyplot as plt
 from sklearn.model_selection import GroupKFold
@@ -71,14 +72,13 @@ def findCoef(x_train,y_train,nN):
         clf.fit(x_train[i],y_train[i])
         co = list(map(lambda x : abs(x),clf.coef_))
         COEF = (sorted(zip(co,[ i for i in range (len(co))]),reverse=True))
-        for i in range(len(COEF)):
-            if(i < (len(COEF)/100)*n ):
-                filtered[COEF[i][1]] = 1 
+        # print(np.max(clf.coef_),np.min(clf.coef_),np.max(co))
+        for j in range(len(COEF)):
+            if(j < (len(COEF)/100)*n ):
+                filtered[COEF[j][1]] = 1 
             else:
-                filtered[COEF[i][1]] = 0
-        # np.save('CVs{}'.format(nN),filtered)
+                filtered[COEF[j][1]] = 0
         coef.append(filtered)
-        #print(COEF[0:10])
     return coef
 
 def findDistance(x):
@@ -117,7 +117,6 @@ def changeIndex2Tag(x,y):
             for k in range(a):
                 if(x[i][j][1] == k):
                     temp.append(y[i][k])
-                
         output.append(temp)
     return output
 
@@ -129,11 +128,11 @@ def score(rounded,x,y):
             if(x[i][j] == y[i][j]):
                 acc = acc+1
         acc = float(acc/len(y[0]))
-        acc = round(acc,4)
+        acc = round(acc,3)
         total.append(acc)
     #print('{},{},{}'.format(rounded,total , round(np.mean(total),3)))
-
     return total,round(np.mean(total),3)
+
 def selectFeature(x,coef):
     output = []
     for i in range(len(x)):
@@ -146,54 +145,38 @@ def selectFeature(x,coef):
             tempSample.append(tempFeature)
         output.append(tempSample)
     return output
-def forUse(t,y):
+
+def xy(x,y,idx):
+    outputX = []
+    outputY = []
+    for index,i in enumerate(y):
+        for j in idx:
+            if(i==j):
+                outputX.append(x[index])
+                outputY.append(y[index])
+    return outputX,outputY
+
+def forUse(x,y):
     allSeed = []
     for count in range(20):
         a,b = getTrainTestIndex(count)
         allSeed.append([a,b])
-
-    All = []
-    for i in range(len(allSeed)):
-        Train = []
-        Test = []
-        for j in range(len(allSeed[0][0])):
-            for k in range(len(y)):
-                if y[k] == allSeed[i][0][j]:
-                    Train.append(k)
-        for l in range(len(allSeed[0][1])):
-            for m in range(len(y)):
-                if y[m] == allSeed[i][1][l]:
-                    Test.append(m)
-        All.append([Train,Test])
-    X_Train = []
-    X_Test = []
-    aX = []
-    for i in range(20):
-        for j in (All[i][0]):
-            X_Train.append(t[i][j])
-        for k in (All[i][1]):
-            X_Test.append(t[i][k])
-        aX.append([X_Train,X_Test])
-        X_Train = []
-        X_Test = []
-    aY = []
-    for i in range(20):
-        y_train = []
-        y_test = []
-        for j in range(len(y[All[i][0]])):
-            y_train.append(y[All[i][0][j]])
-        for j in range(len(y[All[i][1]])):
-            y_test.append(y[All[i][1][j]])
-
-        aY.append([y_train,y_test])
-    return aX,aY
-
+    x_train = []
+    x_test = []
+    y_train = []
+    y_test = []
+    for i,j in allSeed:
+        a,b = xy(x,y,i)
+        c,d = xy(x,y,j)
+        x_train.append(a),y_train.append(b),x_test.append(c),y_test.append(d)
+    return x_train,y_train,x_test,y_test
+    
 def extract(x,y):
     X_train = []
     Y_train = []
     X_test  = []
     Y_test = []
-    for idx,(i,j) in enumerate(zip(x,y)) :
+    for i,j in zip(x,y) :
         X_train.append(i[0])
         X_test.append(i[1])
         Y_train.append(j[0])
@@ -203,85 +186,79 @@ def extract(x,y):
 features_index_path = 'features_index'
 features_path = 'features'
 
-
 rate = [i for i in range(10,101,10)]
 x,y = getFeatureData()
-z = getFeaturesIndex()
-t = mapIndex(x,y,z)
-allX,allY = forUse(t,y) #allX[x][y](feature) =  x is seed , y [0,1] 0 is trian feature , 1 is test || ally[x][y](label) x = is seed , y is index
+X_train,Y_train,X_test,Y_test = forUse(x,y) #allX[x][y](feature) =  x is seed , y [0,1] 0 is trian feature , 1 is test || ally[x][y](label) x = is seed , y is index
+# X_train,Y_train,X_test,Y_test = extract(allX,allY)
+for idx,(x,y)in enumerate(zip(X_test[15:],Y_test[15:])):
+    a1 = []
+    a2 = []
+    a3 = [] #cross validation phase
+    a4 = []
+    a5 = []
+    bb = []
+    cc = []
+    for i in rate :
+        #x is samples have n samples for each sample it have features 2048 features 
+        #y is label have n number
+        x_train = x
+        y_train = y 
+        # x_test = z
+        # Y_test = t
+        x_train,x_test,y_train,y_test = splitXVal(x,y)
+        #x_train,x_test,y_train,y_test foreach object have 5 group inside.
 
-X_train,Y_train,X_test,Y_test = extract(allX,allY)
-
-# for idx,(x,y)in enumerate(zip(X_train[15:],Y_train[15:])):
-#     a1 = []
-#     a2 = []
-#     a3 = [] #cross validation phase
-#     a4 = []
-#     a5 = []
-#     bb = []
-#     cc = []
-#     for i in rate :
-#         #x is samples have n samples for each sample it have features 2048 features 
-#         #y is label have n number
-#         x_train = x
-#         y_train = y 
-#         x_test = z
-#         Y_test = t
-#         x_train,x_test,y_train,y_test = splitXVal(x,y)
-#         #x_train,x_test,y_train,y_test foreach object have 5 group inside.
-
-#         coef = findCoef(x_train,y_train,i)
-#         #z = getFeaturesIndex()
-#         x_test = selectFeature(x_test,coef)
-#         #t = mapIndex(x,y,coef)
-#         distance = findDistance(x_test) #use cosine for every Index
-#         distance = fixPosition(distance) #fix cosine with itself = 0 
-#         closer = findCloser(distance) #find the most likely
-#         closer = changeIndex2Tag(closer,y_test) #change index the closest to label index
-#         a,b = score(i,closer,y_test) #given score to each sample 
-#         a1.append(a[0]),a2.append(a[1]),a3.append(a[2]),a4.append(a[3]),a5.append(a[4]),bb.append(b),cc.append(i)
-#     for i in a1:
-#         print(i)
-#     print('------------------------------')
-#     for i in a2:
-#         print(i)
-#     print('------------------------------')
-#     for i in a3:
-#         print(i)
-#     print('------------------------------')
-#     for i in a4:
-#         print(i)
-#     print('------------------------------')
-#     for i in a5:
-#         print(i)
-#     print('------------------------------')
-#     for i in bb:
-#         print(i)
-#     print('------------------------------')
-#     for i in cc:
-#         print(i)
-#     print('------------------------------')
-
-for i in rate :
-    #x is samples have n samples for each sample it have features 2048 features 
-    #y is label have n number
-    x_train = X_train 
-    y_train = Y_train
-    x_test = X_test #test phase
-    y_test = Y_test
-    #x_train,x_test,y_train,y_test = splitXVal(x,y)
-    #x_train,x_test,y_train,y_test foreach object have 5 group inside.
-
-    coef = findCoef(x_train,y_train,i)
-    #z = getFeaturesIndex()
-    x_test = selectFeature(x_test,coef)
-    #t = mapIndex(x,y,coef)
-    print(len(x_test[0][0]))
-    distance = findDistance(x_test) #use cosine for every Index
-    distance = fixPosition(distance) #fix cosine with itself = 0 
-    closer = findCloser(distance) #find the most likely
-    closer = changeIndex2Tag(closer,y_test) #change index the closest to label index
-    a,b = score(i,closer,y_test) #given score to each sample 
-    for i in a : 
+        coef = findCoef(x_train,y_train,i)
+        #z = getFeaturesIndex()
+        x_test = selectFeature(x_test,coef)
+        #t = mapIndex(x,y,coef)
+        distance = findDistance(x_test) #use cosine for every Index
+        distance = fixPosition(distance) #fix cosine with itself = 0 
+        closer = findCloser(distance) #find the most likely
+        closer = changeIndex2Tag(closer,y_test) #change index the closest to label index
+        a,b = score(i,closer,y_test) #given score to each sample 
+        a1.append(a[0]),a2.append(a[1]),a3.append(a[2]),a4.append(a[3]),a5.append(a[4]),bb.append(b),cc.append(i)
+    for i in a1:
         print(i)
     print('------------------------------')
+    for i in a2:
+        print(i)
+    print('------------------------------')
+    for i in a3:
+        print(i)
+    print('------------------------------')
+    for i in a4:
+        print(i)
+    print('------------------------------')
+    for i in a5:
+        print(i)
+    print('------------------------------')
+    for i in bb:
+        print(i)
+    print('------------------------------')
+    for i in cc:
+        print(i)
+    print('------------------------------')
+
+# for i in rate :
+#     #x is samples have n samples for each sample it have features 2048 features 
+#     #y is label have n number
+#     x_train = X_test
+#     y_train = Y_test
+#     x_test = X_train #test phase
+#     y_test = Y_train
+#     #x_train,x_test,y_train,y_test = splitXVal(x,y)
+#     #x_train,x_test,y_train,y_test foreach object have 5 group inside.
+
+#     coef = findCoef(x_train,y_train,i)
+#     #z = getFeaturesIndex()
+#     x_test = selectFeature(x_test,coef)
+#     #t = mapIndex(x,y,coef)
+#     distance = findDistance(x_test) #use cosine for every Index
+#     distance = fixPosition(distance) #fix cosine with itself = 0 
+#     closer = findCloser(distance) #find the most likely
+#     closer = changeIndex2Tag(closer,y_test) #change index the closest to label index
+#     a,b = score(i,closer,y_test) #given score to each sample 
+#     for i in a : 
+#         print(i)
+#     print('------------------------------')
